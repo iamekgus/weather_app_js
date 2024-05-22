d48bd95d3e220bf782eb08e3cf7dc669b60f0a40
김다현 <kimdahyeon@Kimdahyeons-MacBook-Pro.local>
2024-05-22 12:52:43 +0900

    0522_v0.9

--- js/api.js
+++ js/api.js
@@ -1,22 +1,31 @@
-const api_key = '30f79eac8f2abb1b72a3955b3e52147d'
+const api_key = '30f79eac8f2abb1b72a3955b3e52147d';
 
-export const url = { // endpoints urls
-  currentWeather (lat, lon) {
-    return `https://api.openweathermap.org/data/2.5/weather lat=${lat}&lon=${lon}&units=metric`; 
+export const url = {
+  // endpoint urls
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
   },
 
   geocode(loca) {
-     return `https://api.openweathermap.org/geo/1.0/direct?q=${loca}&limit=5`; 
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${loca}&limit=5`;
+  },
+
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
+  },
+
+airPollution(lat, lon) {
+  return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&units=metric`;
   },
 };
 
 /**
- * 
- * @param {string} url 
- * @param {function} callback 
+ *
+ * @param {string} url
+ * @param {function} callback
  */
 
-export const fetchData = function(url, callback) {
+export const fetchData = function (url, callback) {
   fetch(`${url}&appid=${api_key}`)
     .then((response) => response.json())
     .then((data) => callback(data))
--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-import {url, fetchData} from './api.js';
+import { url, fetchData } from './api.js';
 
 /**
- * 
+ *
  * @param {NodeList} elmts selected elements from HTML to add events
  * @param {string} event event type e.g. 'click', 'mouseenter'...
  * @param {Function} callback callback function to be executed when event is triggered
  */
 
 export function eventOnElmts(elmts, event, callback) {
-  for(const elmt of elmts) {
+  for (const elmt of elmts) {
     elmt.addEventListener(event, callback);
   }
 }
